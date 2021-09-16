373611ba0a296f9e0b3445603a0494b9609518f4
Marsupilami <martin.andrieux59@gmail.com>
2021-09-16 20:14:48 +0200

    comment pmonad

--- src/PMonad.hs
+++ src/PMonad.hs
@@ -1,6 +1,5 @@
 module PMonad where
 
-
 import Control.Monad.State
 import Data.Word
 
@@ -8,11 +7,12 @@ type PState = ( [Word8] -- remaining bytes
               , String  -- remaining string
               )
 
+-- The monad in which all the computations are done
 type P a = StateT PState (Either String) a
 
+-- Run the lexer/parser/converter on the given string
 runP :: P a -> String -> Either String a
 runP p s = evalStateT p ([], s)
-
 
 
 data Value
